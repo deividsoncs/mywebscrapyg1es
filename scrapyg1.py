3d2fcf9cb6fc102cc83f7105692840c11e8e01d0
deividsoncs <deividsoncs@gmail.com>
2018-02-28 21:35:00 -0300

    ajustado broken queries

--- scrapyg1.py
+++ scrapyg1.py
@@ -3,7 +3,7 @@
 """
 Created on Sat Feb 24 16:52:20 2018
 Realiza a captura das notícias do site do G1/ES e insere no banco de dados
-MySQL 
+MySQL
 http://dcalixtoblog.wordpress.com
 @author: calixto
 """
@@ -45,7 +45,7 @@ containers = page_soup.findAll("div", {"class":"bastian-feed-item"})
 #capturo o link da notícia
 #link_container = containers[0].findAll("a" ,{"class":"feed-post-link"})
 #retiro o conteúdo de href
-#link = link_container[0].get('href') 
+#link = link_container[0].get('href')
 #print(link)
 #tags_container = containers[0].findAll("span", {"class":"feed-post-header-chapeu"})
 #print(tags_container[0].prettify())
@@ -58,59 +58,69 @@ clean_notices = list()
 #itero todos containers que adquiri no meu request a página do G1-ES
 for container in containers:
     #pego título da notícia
-    title_container = container.findAll("p", {"class":"feed-post-body-title"})    
-    title_notice = title_container[0].text
-    
+    title_container = container.findAll("p", {"class":"feed-post-body-title"})
+    title_notice=''
+    for tit in title_container:
+        title_notice = tit.text
+        title_notice = title_notice.replace("'", "\\'")
+
     #pego o resumo
     abstract_container = []
     abstract_container = container.findAll("p", {"class":"feed-post-body-resumo"})
-    for abstract in abstract_container:    
+    abstract_notice = ""
+    for abstract in abstract_container:
         abstract_notice = abstract.text
-    
+        abstract_notice = abstract_notice.replace("'", "\\'")
+        print(abstract_notice)
+
     #capturo o link da notícia
-    link_container = container.findAll("a", {"class":"feed-post-link"})    
+    link_container = container.findAll("a", {"class":"feed-post-link"})
     #retiro o conteúdo de href, sempre tenho somente um link nessa classe :P
     link_notice = link_container[0].get('href')
-    
+
     #(TODO)Adicionar função de entrar dentro do link e coletar notícia completa.
     #pego a url da notícia e leio sua página
     my_url=link_notice
     uClient = uReq(my_url)
     page_html = uClient.read()
     uClient.close()
-    
+
     #TODO(Torna tudo isso uma função...)
     #fazendo o parse agora da página do conteúdo
     page_soup = soup(page_html, "html.parser")
     #pego o conteúdo da notícia da página recente que acabamos de visitar
     contents_containers = page_soup.findAll("p", {"class":"content-text__container"})
-    
+
     #pego a data da notícia que não tinha na primeira página
-    data_containers = page_soup.find_all("div", {"class":"content-publication-data"})    
-    try:        
+    data_containers = page_soup.find_all("div", {"class":"content-publication-data"})
+    try:
         for data_container in data_containers:
             #print(data_container.prettify())
             #print(data_container.time.get('datetime'))
             #converto a string da data para datetime depois uso o strftime pra torna o datetime em uma string do formato do datetime do meu banco
-            data_notice = datetime.strptime(data_container.time.get('datetime'), '%Y-%m-%dT%H:%M:%S.%fZ').strftime('\'%Y/%m/%d %I:%M:%S\'')            
+            data_notice = datetime.strptime(data_container.time.get('datetime'), '%Y-%m-%dT%H:%M:%S.%fZ').strftime('\'%Y/%m/%d %I:%M:%S\'')
     except (RuntimeError, TypeError):
         #qualque 'zica' tomo a data como a atual
         data_notice = datetime.now().strftime('\'%Y/%m/%d %I:%M:%S\'')
     #se a minha string está vazia
     if not data_notice:
-        data_notice.now().strftime('\'%Y/%m/%d %I:%M:%S\'')    
+        data_notice.now().strftime('\'%Y/%m/%d %I:%M:%S\'')
     #vou iterar todos contents, (são vários por o G1 Divide a informação em vários
     #o editor deles deve trabalhar dividindo partes do conteúdo em classes CSS)
-    content_notice = ''
+    content_notice = ""
     for content_container in contents_containers:
         #content_notice.join(content_container.text)
         content_notice = content_notice + content_container.text
-    
-    
+        content_notice = content_notice.replace("'", "\\'")
+
+
     #tag é a classificação da notícia segundo site do G1 (Chapéu o.O :P)
-    tags_container = container.findAll("span", {"class":"feed-post-header-chapeu"})    
-    tags_notice = tags_container[0].text
-        
+    tags_container = container.findAll("span", {"class":"feed-post-header-chapeu"})
+    tags_notice = ""
+    for tag in tags_container:
+        tags_notice = tag.text
+        tags_notice = tags_notice.replace("'","\\'")
+
     print(title_notice)#,'\n', abstract_notice, '\n', link_notice, '\n ', tags_notice, '\n', content_notice)
     #preencho meu objeto notícia
     n = Noticia()
@@ -122,12 +132,12 @@ for container in containers:
     #adiciono a lista das noticia tratadas
     clean_notices.append(n)
 
-#(TODO) checar erros    
+#(TODO) checar erros
 if (True):
     con = mySQL.connect(host ='127.0.0.1', user='root', passwd="root1234", db='bd_noticia')
-    try:        
+    try:
         con.select_db("bd_noticia")
-        
+
         #instancia o cursor para execulçao de cmd's
         cursor = con.cursor()
         #força a conexão encodar utf8, por default o connector ira força para latin-1 (8859-1) (instalação)
@@ -136,37 +146,37 @@ if (True):
         cursor.execute('SET NAMES utf8;')
         cursor.execute('SET CHARACTER SET utf8;')
         cursor.execute('SET character_set_connection=utf8;')
-        
+
         ############################################
         #testar a conexao com o banco
         #ver = cursor.execute("SELECT VERSION()")
         #ver = cursor.fetchone()
         #print("Versão do MySQL: %s" % ver)
         ############################################
-        
+
         #usando o for de outra maneira, somente para demonstração
-        for i in range(len(clean_notices)):                        
+        for i in range(len(clean_notices)):
             #verifico se a notícia foi inserida no banco, o link como PK
             sql = (
-                    "SELECT 1 FROM noticia WHERE titulo LIKE \'" + clean_notices[i].title 
-                    +"\' AND link LIKE \'" + clean_notices[i].link +"\'" 
+                    "SELECT 1 FROM noticia WHERE titulo LIKE \'" + clean_notices[i].title
+                    +"\' AND link LIKE \'" + clean_notices[i].link +"\'"
             )
             cursor.execute(sql)
-            num_rows = int(cursor.rowcount)        
+            num_rows = int(cursor.rowcount)
             #print("ROWS: %d" % num_rows)
             #se noticia não existe na base insere.
-            if (num_rows == 0):                
+            if (num_rows == 0):
                 sql = (
-                    "INSERT INTO noticia(titulo, conteudo, link, tags, resumo, data) " 
+                    "INSERT INTO noticia(titulo, conteudo, link, tags, resumo, data) "
                     "VALUES (%s, %s, %s, %s, %s, %s)"
                 )
                 data = (clean_notices[i].title, clean_notices[i].content, clean_notices[i].link, clean_notices[i].tags, clean_notices[i].abstract, clean_notices[i].data)
                 cursor.execute(sql, data)
                 #cursor.execute(sql, ("teste", "teste", "teste", "teste", "teste"))
                 #commita a transação de inserção
-                con.commit()         
+                con.commit()
     except mySQL.Error:
-        print("Erro ao interagir com banco. Erro")        
+        print("Erro ao interagir com banco. Erro")
         raise
     finally:
         if con:
